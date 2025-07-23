0355018c1800fb62da2495904cd5ee4f36f528e5
Korya25 <????? ??? GitHub>
2025-07-23 13:08:38 +0300

    refactor app text style to in clude colors

--- lib/core/resources/app_text_styles.dart
+++ lib/core/resources/app_text_styles.dart
@@ -1,68 +1,70 @@
+import 'package:big_cart/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
   // Titles - Poppins SemiBold
   static TextStyle title15 = GoogleFonts.poppins(
-    fontSize: 15,
+    fontSize: 15.sp,
     fontWeight: FontWeight.w600,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle title20 = GoogleFonts.poppins(
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.w600,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle title25 = GoogleFonts.poppins(
-    fontSize: 25,
+    fontSize: 25.sp,
     fontWeight: FontWeight.w600,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle title30 = GoogleFonts.poppins(
-    fontSize: 30,
+    fontSize: 30.sp,
     fontWeight: FontWeight.w600,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   // Paragraph - Regular
   static TextStyle paragraphRegular15 = GoogleFonts.poppins(
-    fontSize: 15,
+    fontSize: 15.sp,
     fontWeight: FontWeight.w400,
-    color: Colors.black,
+    color: AppColors.textGrey,
   );
 
   // Paragraph - Medium
   static TextStyle paragraphMedium10 = GoogleFonts.poppins(
-    fontSize: 10,
+    fontSize: 10.sp,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle paragraphMedium12 = GoogleFonts.poppins(
-    fontSize: 12,
+    fontSize: 12.sp,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle paragraphMedium15 = GoogleFonts.poppins(
-    fontSize: 15,
+    fontSize: 15.sp,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   // Label
   static TextStyle label12 = GoogleFonts.poppins(
-    fontSize: 12,
+    fontSize: 12.sp,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 
   static TextStyle label15 = GoogleFonts.poppins(
-    fontSize: 15,
+    fontSize: 15.sp,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.textBlack,
   );
 }
