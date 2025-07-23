38bf2d9de00d643bb22923407632eb13b8fa878a
Korya25 <????? ??? GitHub>
2025-07-23 12:35:40 +0300

    add app assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,20 @@
+class AppAssets {
+  // App Logo
+  static const String appLogoSvg = 'assets/app_icon_svgs/bigCart 1.svg';
+
+  // Category SVGs
+  static const String beveragesSvg = 'assets/Categories_svgs/beverage 1.svg';
+  static const String fruitsSvg = 'assets/Categories_svgs/Group (1).svg';
+  static const String grocerySvg = 'assets/Categories_svgs/Group (2).svg';
+  static const String edibleOilSvg = 'assets/Categories_svgs/Group (3).svg';
+  static const String vegetablesSvg = 'assets/Categories_svgs/Group.svg';
+  static const String householdSvg = 'assets/Categories_svgs/vacuum 1.svg';
+
+  // Onboarding PNGs
+  static const List<String> onboardingPngs = [
+    'assets/onboarding/onboarding_1.png',
+    'assets/onboarding/onboarding_2.png',
+    'assets/onboarding/onboarding_3.png',
+    'assets/onboarding/onboarding_4.png',
+  ];
+}
