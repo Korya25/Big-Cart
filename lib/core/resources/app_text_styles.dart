0c19ae55a170ab176e41bdb122ef73ab0cafb940
Korya25 <????? ??? GitHub>
2025-07-23 13:02:32 +0300

    add app text styles file

---
+++ lib/core/resources/app_text_styles.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // Titles - Poppins SemiBold
+  static TextStyle title15 = GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  static TextStyle title20 = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  static TextStyle title25 = GoogleFonts.poppins(
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  static TextStyle title30 = GoogleFonts.poppins(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  // Paragraph - Regular
+  static TextStyle paragraphRegular15 = GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  // Paragraph - Medium
+  static TextStyle paragraphMedium10 = GoogleFonts.poppins(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static TextStyle paragraphMedium12 = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static TextStyle paragraphMedium15 = GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  // Label
+  static TextStyle label12 = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static TextStyle label15 = GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+}
