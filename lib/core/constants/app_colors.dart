5765d616cac3859d670aa8d05d8c1d2393786cf0
Korya25 <????? ??? GitHub>
2025-07-23 12:39:54 +0300

    add app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Backgrounds
+  static const Color backgroundWhite = Color(0xFFFFFFFF);
+  static const Color backgroundLightGrey = Color(0xFFF4F5F9);
+  static const Color backgroundGrey = Color(0xFFF5F5F5);
+
+  // Text
+  static const Color textGrey = Color(0xFF868889);
+  static const Color textBlack = Color(0xFF000000);
+
+  // Links
+  static const Color linkBlue = Color(0xFF1A0DAB);
+
+  // Border
+  static const Color borderGrey = Color(0xFFEBEBEB);
+
+  // Primary colors
+  static const Color primary = Color(0xFFAEDC81);
+  static const Color primaryDark = Color(0xFF6CC51D);
+  static const Color primaryLight = Color(0xFFEBFFD7);
+}
