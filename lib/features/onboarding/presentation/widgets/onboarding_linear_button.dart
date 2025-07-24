26782949f5ee6b43a8ace777d74cae63d1dd12a1
Korya25 <????? ??? GitHub>
2025-07-24 13:51:24 +0300

    finish onboreading

--- lib/features/onboarding/presentation/widgets/onboarding_linear_button.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_linear_button.dart
@@ -19,7 +19,7 @@ class OnboardingLinearButton extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
-        height: 60.h,
+        height: 50.h,
         width: 0.8.sw,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(4.r),
